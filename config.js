ae720d34ff65cf7f95f7ed181fe0e3bf38c49f8f
Surya512165 <suryaproject73@gmail.com>
2026-08-13 13:42:14 +0700

    Isi konfigurasi Cloudflare (workerUrl & turnstileSiteKey)

--- config.js
+++ config.js
@@ -4,8 +4,8 @@
 var CF_CONFIG = {
   // GANTI setelah deploy Worker dengan perintah `npx wrangler deploy`
   // Contoh: "https://smanimori-db.your-subdomain.workers.dev"
-  workerUrl: "https://YOUR-WORKER-NAME.your-subdomain.workers.dev",
+  workerUrl: "https://smanimori-db.osissmanimori.workers.dev",
 
   // GANTI dengan "Site Key" dari dashboard Cloudflare -> Turnstile
-  turnstileSiteKey: "0x4AAAAAAA..."
+  turnstileSiteKey: "0x4AAAAAAEOsD7qQdLI66ibN"
 };
